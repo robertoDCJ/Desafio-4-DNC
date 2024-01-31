6489dd905630e685b82db7f412ca104247976697
robertoDCJ <robertox70@hotmail.com>
2024-01-31 13:26:33 -0300

    ajustado o index.scss do componente Background para se adaptar a todo tamanho de tela cobrindo todo o fundo

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -3,8 +3,8 @@ import "./index.scss"
 const Background = () => {
     return (
         <div className="Background">
-            <video autoPlay loop muted>
-                <source src="../../src/assets/chuva.mp4" type="video/mp4" />
+            <video className="Background__video" autoPlay loop muted>
+                <source src="../../src/assets/vinil.mp4" type="video/mp4" />
             </video>
         </div>
     )
