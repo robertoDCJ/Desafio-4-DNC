186b88b5826e663a9b1e78e0524d595066c14971
robertoDCJ <robertox70@hotmail.com>
2024-02-01 13:09:21 -0300

    adicionado primeira logica no Tema.jsx

--- src/components/Tema/Tema.jsx
+++ src/components/Tema/Tema.jsx
@@ -1,8 +1,19 @@
+import { useCallback, useState } from "react"
 import "./index.scss"
 
 const Tema = () => {
+    const [tema, setTema] = useState(true)
+
+    const changeTheme = useCallback(
+        () => {
+            setTema(!tema)
+        }, [tema]
+    )
+
+    console.log(tema);
+
     return (
-        <div className="Tema">
+        <div className="Tema" onClick={changeTheme}>
             Alterar Tema
         </div>
     )
